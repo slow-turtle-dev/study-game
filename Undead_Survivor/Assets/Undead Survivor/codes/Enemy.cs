7e730d0885ee115e33fcc30548bee8b2f7813a48
sglee8293 <sglee8293@gmail.com>
2025-04-07 20:56:27 +0900

    몬스터 피격, 죽음, 경험치, 레벨, 킬수 구현

--- Undead_Survivor/Assets/Undead Survivor/codes/Enemy.cs
+++ Undead_Survivor/Assets/Undead Survivor/codes/Enemy.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Threading;
 using UnityEngine;
 
@@ -12,19 +13,23 @@ public class Enemy : MonoBehaviour
     bool isLive;
 
     Rigidbody2D rigid;
+    Collider2D coll;
     Animator anim;
     SpriteRenderer spriter;
+    WaitForFixedUpdate wait;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        coll = GetComponent<Collider2D>();
         anim = GetComponent<Animator>();
         spriter = GetComponent<SpriteRenderer>();
+        wait = new WaitForFixedUpdate();
     }
 
     private void FixedUpdate()
     {
-        if (!isLive) {
+        if (!isLive || anim.GetCurrentAnimatorStateInfo(0).IsName("Hit")) {
             return;
         }
 
@@ -47,6 +52,10 @@ public class Enemy : MonoBehaviour
     {
         target = GameManager.instance.player.GetComponent<Rigidbody2D>();
         isLive = true;
+        coll.enabled = true;
+        rigid.simulated = true;
+        spriter.sortingOrder = 2;
+        anim.SetBool("Dead", false);
         health = maxHealth;
     }
 
@@ -60,19 +69,35 @@ public class Enemy : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if (!collision.CompareTag("Bullet")) {
+        if (!collision.CompareTag("Bullet") || !isLive) {
             return;
         }
 
         health -= collision.GetComponent<Bullet>().damage;
+        StartCoroutine(KnockBack());
 
         if (health > 0) {
             // .. Live, Hit Action
+            anim.SetTrigger("Hit");
         }
         else {
             // .. Die
-            Dead();
+            isLive = false;
+            coll.enabled = false;
+            rigid.simulated = false;
+            spriter.sortingOrder = 1;
+            anim.SetBool("Dead", true);
+            GameManager.instance.kill++;
+            GameManager.instance.GetExp();
         }
+    }
+
+    IEnumerator KnockBack()
+    {
+        yield return wait;  // 다음 하나의 물리 프레임 딜래이
+        Vector3 playerPos = GameManager.instance.player.transform.position;
+        Vector3 dirVec = transform.position - playerPos;
+        rigid.AddForce(dirVec.normalized * 3, ForceMode2D.Impulse);    
     }
 
     void Dead()
