d701aef2003aa7fbbe6993e1ec04ae6c03c3315e
sglee8293 <sglee8293@gmail.com>
2025-04-10 19:55:23 +0900

    능력 업그레이드 구현

--- Undead_Survivor/Assets/Undead Survivor/codes/Weapon.cs
+++ Undead_Survivor/Assets/Undead Survivor/codes/Weapon.cs
@@ -13,12 +13,7 @@ public class Weapon : MonoBehaviour
 
     void Awake()
     {
-        player = GetComponentInParent<Player>();
-    }
-
-    void Start()
-    {
-        Init();
+        player = GameManager.instance.player;
     }
 
     void Update()
@@ -52,10 +47,29 @@ public class Weapon : MonoBehaviour
         if (id == 0) {
             Batch();
         }
+
+        player.BroadcastMessage("ApplyGear", SendMessageOptions.DontRequireReceiver);
     }
 
-    public void Init()
+    public void Init(ItemData data)
     {
+        // Basic Set
+        name = "Weapon " + data.itemId;
+        transform.parent = player.transform;
+        transform.localPosition = Vector3.zero;
+
+        // Property Set
+        id = data.itemId;
+        damage = data.baseDamage;
+        count = data.baseCount;
+
+        for (int index = 0; index < GameManager.instance.pool.prefabs.Length; index++) {
+            if (data.projectile == GameManager.instance.pool.prefabs[index]) {
+                prefabId = index;
+                break;
+            }
+        }
+
         switch (id)
         {
             case 0:
@@ -63,9 +77,11 @@ public class Weapon : MonoBehaviour
                 Batch();
                 break;
             default:
-                speed = 0.3f;
+                speed = 0.4f;
                 break;
         }
+
+        player.BroadcastMessage("ApplyGear", SendMessageOptions.DontRequireReceiver);
     }
 
     void Batch()
