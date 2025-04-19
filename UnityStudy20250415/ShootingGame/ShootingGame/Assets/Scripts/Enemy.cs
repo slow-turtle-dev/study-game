763915cb4abe35cb7e64c62cb165abde5358949e
sglee8293 <sglee8293@gmail.com>
2025-04-19 22:13:48 +0900

    공부중..

---
+++ UnityStudy20250415/ShootingGame/ShootingGame/Assets/Scripts/Enemy.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // 필요 속성: 이동 속도
+    public float speed = 5;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 1. 방향을 구한다
+        Vector3 dir = Vector3.down;
+        // 2. 이동하고 싶다. 공식 P = PO + vt
+        transform.position += dir * speed * Time.deltaTime;
+    }
+}
