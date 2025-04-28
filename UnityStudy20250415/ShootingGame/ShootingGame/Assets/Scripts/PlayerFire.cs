ea234818d9d20a8bef71a67dc44fd4403f11e8be
sglee8293 <sglee8293@gmail.com>
2025-04-28 22:14:59 +0900

    조이스틱 구현

--- UnityStudy20250415/ShootingGame/ShootingGame/Assets/Scripts/PlayerFire.cs
+++ UnityStudy20250415/ShootingGame/ShootingGame/Assets/Scripts/PlayerFire.cs
@@ -29,27 +29,42 @@ public class PlayerFire : MonoBehaviour
             // 비활성화시키자.
             bullet.SetActive(false);
         }
+
+        // 실행되는 플랫폼이 안드로이드일 경우 조이스틱을 활성화시킨다.
+        #if UNITY_ANDROID
+                GameObject.Find("Joystick canvas XYBZ").SetActive(true);
+        #elif   UNITY_EDITOR || UNITY_STANDALONE
+                GameObject.Find("Joystick canvas XYBZ").SetActive(false);
+        #endif
     }
 
     // 목표: 발사 버튼을 누르면 탄창에 있는 총알 중 비활성화된 것을 발사하고 싶다.
     void Update()
     {
-        // 목표: 사용자가 발사 버튼을 누르면 총알을 발사하고 싶다.
-        // 순서: 1. 사용자가 발사 버튼을 누르면
-        // 만약 사용자가 발사 버튼을 누르면
-        if (Input.GetButtonDown("Fire1")) {
-            // 2. 탄창 안에 있는 총알이 있다면
-            if (bulletObjectPool.Count > 0) {
-                // 3. 비활성화된 총알을 하나 가져온다.
-                GameObject bullet = bulletObjectPool[0];
-                // 4. 총알을 발사하고 싶다(활성화시킨다).
-                bullet.SetActive(true);
-                // 오브젝트 풀에서 총알 제거
-                bulletObjectPool.Remove(bullet);
-
-                // 총알을 위치시키기
-                bullet.transform.position = transform.position;
+        // 유니티 데이터와 PC(Mac, Windows, Linux) 환경일 때 동작
+        #if UNITY_EDITOR || UNITY_STANDALONE
+            // 목표: 사용자가 발사 버튼을 누르면 총알을 발사하고 싶다.
+            // 순서: 1. 사용자가 발사 버튼을 누르면
+            // 만약 사용자가 발사 버튼을 누르면
+            if (Input.GetButtonDown("Fire1")) {
+                Fire();
             }
+        #endif
+    }
+
+    public void Fire()
+    {
+        // 2. 탄창 안에 있는 총알이 있다면
+        if (bulletObjectPool.Count > 0) {
+            // 3. 비활성화된 총알을 하나 가져온다.
+            GameObject bullet = bulletObjectPool[0];
+            // 4. 총알을 발사하고 싶다(활성화시킨다).
+            bullet.SetActive(true);
+            // 오브젝트 풀에서 총알 제거
+            bulletObjectPool.Remove(bullet);
+
+            // 총알을 위치시키기
+            bullet.transform.position = transform.position;
         }
     }
 }
