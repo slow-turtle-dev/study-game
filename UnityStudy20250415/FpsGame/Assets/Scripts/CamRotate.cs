b4cdcfce118d8af619ae8ec04c06333d7d1c3150
sglee8293 <sglee8293@gmail.com>
2025-05-02 23:14:01 +0900

    카메라 회전, 플레이어 회전 구현중

--- UnityStudy20250415/FpsGame/Assets/Scripts/CamRotate.cs
+++ UnityStudy20250415/FpsGame/Assets/Scripts/CamRotate.cs
@@ -5,6 +5,10 @@ public class CamRotate : MonoBehaviour
     // 회전 속도 변수
     public float rotSpeed = 200f;
 
+    // 회전 값 변수
+    float mx = 0;
+    float my = 0;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -20,6 +24,17 @@ public class CamRotate : MonoBehaviour
         float mouse_X = Input.GetAxis("Mouse X");
         float mouse_Y = Input.GetAxis("Mouse Y");
 
+        // 1-1. 회전 값 변수에 마우스 입력 값만큼 미리 누적시킨다.
+        mx += mouse_X * rotSpeed * Time.deltaTime;
+        my += mouse_Y * rotSpeed * Time.deltaTime;
+
+        // 1-2. 마우스 상하 이동 회전 변수(my)의 값을 -90도~90도 사이로 제한한다.
+        my = Mathf.Clamp(my, -90f, 90f);
+
+        // 2. 물체를 회전 방향으로 회전시킨다.
+        transform.eulerAngles = new Vector3(-my, mx, 0);
+        
+        /*
         // 2. 마우스 입력 값을 이용해 회전 방향을 결정한다.
         // 좌우 회전 시는 y축, 상하 회전 시는 x 축
         // x축은 양수는 아래로, 음수는 위로 회전하므로 마우스 입력값의 반대로 적용
@@ -33,5 +48,6 @@ public class CamRotate : MonoBehaviour
         Vector3 rot = transform.eulerAngles;
         rot.x = Mathf.Clamp(rot.x, -90f, 90f);
         transform.eulerAngles = rot;
+        */
     }
 }
